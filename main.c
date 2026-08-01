40d3fbf5059955bc19e2f82e38b6f8ec7c5a9c28
flong69zxc-max <flong69zxc@gmail.com>
2026-08-01 17:59:56 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-// Функция выполнится при инъекции библиотеки в игру
 __attribute__((constructor)) void entry() {
-    printf("=== MOD LOADED SUCCESSFULLY ===\n");
+    // Ждем пару секунд пока игра прогрузит UI, затем показываем окно
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Успех!" 
+                                                                   message:@"Твоя dylib успешно загрузилась в игру!" 
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+        
+        [alert addAction:[UIAlertAction actionWithTitle:@"ОК" style:UIAlertActionStyleDefault handler:nil]];
+        
+        // Получаем корневое окно и показываем алерт
+        UIWindow *window = [[[UIApplication sharedApplication] windows] firstObject];
+        [window.rootViewController presentViewController:alert animated:YES completion:nil];
+    });
 }
